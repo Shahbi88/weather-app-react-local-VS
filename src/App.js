268c1faea8c04ca707d329ac516f5d38e3aadc6e
Shahbi88 <rihabazar@gmail.com>
2023-12-13 14:57:24 +0000

    added axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello World!</h1>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
